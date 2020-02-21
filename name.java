fcae8260f2366e57de4ff6a81df149c3ce69ee1f
radeefchowdhury <60351584+radeefchowdhury@users.noreply.github.com>
2020-02-21 10:11:14 -0500

    Update name.java

--- name.java
+++ name.java
@@ -3,12 +3,14 @@ package name;
 import java.util.Scanner;
 
 public class name {
-    
+    /*This changes the name of the person removing the comma and putting upper case on the first letters*/
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your first and last name");
-        String firstname = sc.nextLine();
-        String lastname = sc.nextLine();
+        System.out.println("Enter your name in the format: Last,First");
+        String fullname = sc.nextLine();
+        int index = fullname.indexOf(',');
+        String lastname = fullname.substring(0,index);
+        String firstname = fullname.substring(index,2);
         lastname = lastname.substring(0,1).toUpperCase() + lastname.substring(1).toLowerCase();
         firstname = firstname.substring(0,1).toUpperCase() + firstname.substring(1).toLowerCase();
          System.out.println("Your full name is: " + firstname +  lastname);    
